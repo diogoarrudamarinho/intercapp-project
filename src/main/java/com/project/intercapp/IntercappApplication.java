aa68028d4550b2fe122f15d29270a00e8671bda7
Diogo Arruda <diogo.arrudamarinho@gmail.com>
2024-11-27 15:14:48 -0300

    Project created

---
+++ src/main/java/com/project/intercapp/IntercappApplication.java
@@ -0,0 +1,13 @@
+package com.project.intercapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntercappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntercappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/intercapp/IntercappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.intercapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntercappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
