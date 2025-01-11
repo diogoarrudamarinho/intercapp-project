e835d4513a8e9df634d9a2e047465f63b9c7568c
Diogo Arruda <diogo.arrudamarinho@gmail.com>
2025-01-11 17:32:15 -0300

    feat: find by bus id

--- src/main/java/com/project/intercapp/repositories/SeatRepository.java
+++ src/main/java/com/project/intercapp/repositories/SeatRepository.java
@@ -1,8 +1,12 @@
 package com.project.intercapp.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+
 import com.project.intercapp.entities.Seat;
 
 public interface SeatRepository extends JpaRepository<Seat, Long>{
     
+    List<Seat> findByBusId(Long busId);
 }
