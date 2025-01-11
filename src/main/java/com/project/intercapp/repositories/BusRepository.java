c0b4d89ae4cf97e73c53ec132f27e01ac7f93961
Diogo Arruda <diogo.arrudamarinho@gmail.com>
2025-01-11 17:31:33 -0300

    feat: find by plate

--- src/main/java/com/project/intercapp/repositories/BusRepository.java
+++ src/main/java/com/project/intercapp/repositories/BusRepository.java
@@ -1,9 +1,13 @@
 package com.project.intercapp.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+
 import com.project.intercapp.entities.Bus;
 
 public interface BusRepository extends JpaRepository<Bus, Long>{
     
+    Optional<Bus> findByPlate(String plate);
     boolean existsByPlate(String plate);
 }
